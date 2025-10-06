8eef21e9f4126a4b00f5f8fcd49a7d698c5c4cd8
Zhikang Liu <zhikangliu@hotmail.com>
2025-10-07 03:04:55 +0800

    Update .gitignore files to comment out generated API code paths for clarity and maintainability.

---
+++ packages/typescript/src/lib/api/generated/models/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppendData';
+export * from './AppendResult';
+export * from './Content';
+export * from './HTTPValidationError';
+export * from './HealthResponse';
+export * from './MemoryLibraryCreationData';
+export * from './MemoryLibraryGetRequest';
+export * from './MemoryLibraryInformation';
+export * from './MemoryLibraryList';
+export * from './Message';
+export * from './OutboundMessage';
+export * from './QueryData';
+export * from './QueryResult';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
